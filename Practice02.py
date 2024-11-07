bd233183aa47d5cbc84c44180e14d7682aa7eeb3
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-07 12:23:03 +0530

    Practice 02

---
+++ Practice02.py
@@ -0,0 +1,36 @@
+# Exercise 03
+print()
+print("Hello World...!")
+print('Hello World...!')
+print()
+print("Hello World...!")
+print(5)
+
+# Exercise 04
+print()
+print("First Program - Python print function")
+print("It is declared like this:")
+print("print('What to print')")
+print('print("What to print")')
+# print('print('What to print')')
+print()
+
+# Exercise 05
+print("Hello World\nHello World\nHello World")
+print("hello World\nhello World\nhello World")
+print("Hello" + "Jenny")
+print("Hello " + "Jenny")
+print("Hello" + " Jenny")
+print("Hello" + " " + "Jenny")
+print()
+
+# Exercise 06
+print("String Manipulation Exercise")
+print('String Concatenation is done with "+" Sign')
+print('e.g. print("Hello" + "Jenny")')
+print("New lines can be created with a backslash and n")
+print()
+print("String Manipulation Exercise" +
+      '\nString Concatenation is done with "+" Sign' +
+      '\ne.g. print("Hello" + "Jenny")' +
+      "\nNew lines can be created with a backslash and n")
