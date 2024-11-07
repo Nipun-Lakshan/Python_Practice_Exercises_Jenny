dce906ae7668ebf0ab05f08943c99c22293582fa
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-07 14:36:11 +0530

    Practice 03

--- Practice03.py
+++ Practice03.py
@@ -1,1 +1,13 @@
 # Exercise 07
+print()
+print("hello World...")
+print()
+input("What is your name? ")
+print()
+print("Hello" + " " + "Jenny\n")
+print("Hello" + " " + input("What is your name? "))
+print()
+print("Hey " + input("What is your name? ") + ", " + "how are you? ")
+input()
+print("Hello " + input("What is your name? "))
+print()
