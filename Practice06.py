2509bd8cd31c88dadbcd79845647926d615c3d3e
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-08 19:01:46 +0530

    Practice 06

--- Practice06.py
+++ Practice06.py
@@ -1,1 +1,29 @@
 # Exercise 10
+name = input("What is your name? ")  # Meaningful, No Error
+a = input("What is your name? ")  # Unmeaningful, No Error
+name = input("What is your age? ")  # Unmeaningful, No Error
+# 1_age = 5 -> Variable name should be started with either letter or underscore
+age_1 = 22
+_age = 22
+print("\n" + str(age_1))
+print(str(_age) + "\n")
+myName = "Jenny"
+MyVariableName = "Jenny"
+MyName = "Jenny"
+my_variable_name = "Jenny"
+print(myName)
+print(MyVariableName)
+print(MyName)
+print(my_variable_name + "\n")
+var = 5
+print(var)
+a = 1
+print(a)
+print()
+# 2 = a
+var_a = 1
+print(var_a)
+Roll_No = 25
+roll_No = 26
+print(Roll_No)
+print(roll_No)
