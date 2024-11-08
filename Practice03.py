a99a6affd685c6a32c9a1b6f768114c59cd5c447
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-08 13:38:27 +0530

    Practice 05

--- Practice03.py
+++ Practice03.py
@@ -1,3 +1,4 @@
+# Exercise 07
 print()
 print("hello World...")
 print()
--- Practice05.py
+++ Practice05.py
@@ -1,1 +1,10 @@
 # Exercise 09
+a = input("\nenter value of a = ")
+b = input("enter value of b = ")
+temp = a
+a = b
+b = temp
+print("\na = " + str(a))
+print("b = " + str(b))
+print("\na = ", a)
+print("b = ", b)
