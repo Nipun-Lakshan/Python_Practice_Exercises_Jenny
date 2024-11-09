f6f2c853e92e5f41bd7350e77248e6bccf69d583
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-09 11:58:18 +0530

    Practice 08

--- Practice08.py
+++ Practice08.py
@@ -1,1 +1,36 @@
 # Exercise 12
+print()
+var_1 = 123456789123456789789
+print(var_1)
+print(var_1 + 1)
+print()
+var_1 = 123
+var_2 = 10.1
+print(var_1 + var_2)
+print(type(var_1))
+print(type(var_2))
+print()
+var_1 = 0o123
+var_2 = 0x123
+print(var_1)  # Print 83
+print(var_2)  # Print 291
+print()
+print("Jenny Khatri")
+print('Jenny Khatri')
+print()
+name = "Jenny Kharti"
+print(type(name))
+print(name[0])
+print(name[5])
+print(name[7])
+print("Length of Name :- " + str(len(name)))
+print()
+name_1 = "Jenny Kharti"
+name_2 = "CS Faculty"
+a = 1
+print(name_1 + name_2)
+name_1 = "123"
+# print(name_1 + a)
+# String + int Concatenation Error
+print(name_1 + name_2)
+print()
---
+++ Practice09.py
@@ -0,0 +1,33 @@
+# Exercise 13
+print()
+print(123 + 1)  # Print 124
+print("123" + "1")  # Print 1231
+print()
+print("Jenny's Lectures")  # Same Output
+print("Jenny\'s Lectures")  # Same Output
+print("Jenny's \"Lectures\"")
+print()
+print("Jenny's \n\"Lectures\"")  # Giving Speciality
+print("Jenny's \\n\"Lectures\"")  # Removing Speciality
+print(5 * "Jenny ")  # Print Jenny 5 times in same row
+print()
+var = True
+print(var)
+print(type(var))
+# var = true // Name Error
+print()
+a = 1
+b = 2
+var = a < b
+print(var)
+print(type(var))
+var = '''Start'''  # Define string in triple quote
+print(var)
+print()
+name = "Jenny's Lecture \"CS/IT\""
+print(name)
+print()
+print(5 * "Jenny")  # JennyJennyJennyJennyJenny
+print("100" + "100")
+print(100 + 100)
+print()
