ff7384e541b2f2f3423c702bb03b5e1bd3c2a588
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-09 14:00:46 +0530

    Practice 11

--- Practice11.py
+++ Practice11.py
@@ -1,1 +1,7 @@
 # Exercise 16
+print()
+number = input("Enter two digits number (00 to 99) :- ")
+# number is a string
+num1 = int(number[0])
+num2 = int(number[1])
+print("Addition of Two Digits :- " + str(num1 + num2))
