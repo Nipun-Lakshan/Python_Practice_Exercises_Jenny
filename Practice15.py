4d0099ae0843e515f5014b4bccaf086339b07692
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-10 01:18:06 +0530

    Practice 15

--- Practice15.py
+++ Practice15.py
@@ -1,2 +1,36 @@
 # Exercise 20
 print()
+a, b = 1, 2
+c = a + b
+print(c)  # 3
+c += a
+print(c)  # 4
+c %= b
+print(c)  # 0
+c //= a
+print(c)  # 0
+
+# Exercise 21
+print()
+print(5 == 5)  # True
+print(5 != 6)  # True
+print(6 > 5)  # True
+print(5 < 6)  # True
+print(5 >= 5)  # True
+print(5 <= 6)  # True
+
+print()
+a = 5
+print(a < 5)  # False
+print(a == 5)  # True
+print(a > 5)  # False
+print()
+print(a <= 5)  # True
+print(a != 5)  # False
+print(a != 6)  # True
+print()
+print(a > 6)  # False
+print(a >= 6)  # False
+print(a >= 5)  # True
+print()
+print((a + 1) != 6)  # False
