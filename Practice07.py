756bb1a17fb6e798a46d3407c2b4cd5d48bb8602
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-09 11:29:41 +0530

    Practice 07

--- Practice07.py
+++ Practice07.py
@@ -1,1 +1,32 @@
 # Exercise 11
+print()
+var_1 = 1234567891234567891234567897789639
+print(var_1)
+print(type(var_1))
+print()
+print(0b1010)  # Print 10, Represented in Binary
+print(0B1010)  # Print 10, Represented in Binary
+print()
+print(0o12)  # Print 10, Represented in Octal
+print(0O12)  # Print 10, Represented in Octal
+print()
+print(0xA)  # Print 10, Represented in Hexadecimal
+print(0XA)  # Print 10, Represented in Hexadecimal
+print()
+print(0b10)  # Print 2, Represented in Binary
+print(0o10)  # Print 8, Represented in Octal
+print()
+print('Jenny\'s Lectures')  # String Representation 01
+print("Jenny's Lectures")  # String Representation 02
+print('''Jenny's Lectures''')  # String Representation 03
+print("""Jenny's Lectures""")  # String Representation 04
+print()
+var_1 = None
+print(var_1)
+print()
+print("new\nline")  # Escape Characters 01
+print("tab\ttab")  # Escape Characters 02
+print("\'")  # Escape Characters 03
+print("\"")  # Escape Characters 04
+print("\\n")  # Escape Characters 05
+print()
