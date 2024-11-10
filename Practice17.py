a1e65eab0106e09fe44c168172892ae1de0f133e
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-10 14:56:50 +0530

    Practice 17

--- Practice17.py
+++ Practice17.py
@@ -1,2 +1,25 @@
 # Exercise 23
 print()
+print(5 & 4)  # 4
+print(5 | 4)  # 5
+print(5 ^ 4)  # 1
+print(~5)  # -6
+print(5 << 2)  # 20
+print(5 >> 2)  # 1
+
+print()
+a, b = 5, 4
+print(a & b)  # 4
+print(a | b)  # 5
+print(a ^ b)  # 1
+print(~a)  # -6
+print(a << 2)  # 20
+print(a >> 2)  # 1
+
+print()
+print(26 & 23)  # 18
+print(17 | 24)  # 25
+print(17 ^ 24)  # 9
+print(~45)  # -46
+print(68 << 2)  # 272
+print(56 >> 2)  # 14
