b78f35a1a36cd74c5feea02dcf1b7c0728503225
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-12 00:24:13 +0530

    Practice 25

--- Practice25.py
+++ Practice25.py
@@ -1,1 +1,7 @@
+# Coding Exercise 05
 print()
+current_age = input("Enter your current age: - ")
+remaining_years = 90 - int(current_age)
+print(
+    f"You have {remaining_years * 365} days, {remaining_years * 52} weeks and {remaining_years * 12} months left."
+)
