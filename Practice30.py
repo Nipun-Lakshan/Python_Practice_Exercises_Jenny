4176bc6f5049d3237e1d44c3ed388e11c0193f4d
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-12 23:05:19 +0530

    Practice 30

--- Practice30.py
+++ Practice30.py
@@ -1,1 +1,18 @@
+# Coding Exercise 07
 print()
+print("BMI Calculator")
+print("==============")
+print()
+weight = float(input("Enter your weight in Kg: - "))
+height = float(input("Enter your height in m: - "))
+print()
+bmi_value = round(weight / (height**2))
+print("Your BMI Value: - " + str(bmi_value))
+if (bmi_value < 18.5):
+    print("Result: - Underweight")
+elif (bmi_value <= 24.9):
+    print("Result: - Normal Weight")
+elif (bmi_value <= 29.9):
+    print("Result: - Overweight")
+else:
+    print("Result: - Obese")
