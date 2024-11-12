8459a68476c74ac049ccd029da946d943431f919
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-12 15:36:16 +0530

    Practice 27

---
+++ Practice27.py
@@ -0,0 +1,10 @@
+# Coding Exercise 06
+print()
+print("Checking If a Number is Even or Odd")
+print("===================================")
+print()
+number = int(input("Enter the number: - "))
+if (number % 2 == 0):
+    print(f"{number} is an even number!")
+else:
+    print(f"{number} is an odd number!")
