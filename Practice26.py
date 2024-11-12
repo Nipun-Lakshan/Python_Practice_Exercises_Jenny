d07747efd35fb20d74bb8596c75e51b4b6446502
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-12 11:59:56 +0530

    Practice 26

---
+++ Practice26.py
@@ -0,0 +1,27 @@
+# If & If - Else Statements
+print()
+
+# Example 01
+
+height = int(input("Enter your height in feet: - "))
+if (height > 3):
+    print("Buy Token")
+    print("Now you can board to the metro!")
+else:
+    print("No Token Required!")
+
+# Example 02
+print()
+
+height = int(input("Enter your height in feet: - "))
+if (height > 3):
+    print("Buy Token")
+else:
+    print("No Token Required!")
+
+# Example 03
+print()
+
+income = int(input("Enter your monthly income: - "))
+if (income < 35000):
+    print("You are eligible for scholarship!")
