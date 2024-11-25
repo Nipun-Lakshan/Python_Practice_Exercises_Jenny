b69c6a7c00aec937009c49106a176cfaa49dc0d8
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-11-25 23:15:14 +0530

    Practice 53

--- Practice53.py
+++ Practice53.py
@@ -21,4 +21,31 @@ print()
 
 # Example 03
 
-#############Start Here
+set1 = {'Ram', 'Shyam', 'Jenny'}
+set2 = {'Jiya', 'Aakash'}
+
+print(set1.issubset(['Mohan', 'Shiva', 'Jenny'])) # False
+print(set1.issubset(('Shiva', 'Jenny', 'Ram', 'Shyam'))) # True
+print(set1.issubset(set2)) # False
+print(set1 <= set2) # False
+print(set1 <= set1) # True
+print()
+
+# Example 04
+
+set1 = {'Ram', 'Shyam', 'Jenny', 'Jiya', 'Aakash'}
+set2 = {'Jiya', 'Aakash'}
+print(set2 <= set1) # True
+print(set1.issuperset(set2)) # True
+print(set1 >= set2) # True
+print(set2.issuperset(set1)) # False
+print(set2 >= set1) # False
+print(set1.issuperset(set1)) # True
+print(set1 >= set2) # True
+set2.clear()
+print(set2) # set()
+del set2
+# print(set2) // Not exists a set2 after del statement
+print()
+
+
